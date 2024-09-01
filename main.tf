8579dc5fca4427df632c407280f89939ec553a60
davidchagalidze <chagalidze@gmail.com>
2024-09-01 15:18:06 +0400

    .

--- main.tf
+++ main.tf
@@ -61,11 +61,11 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.alb.target_group_arns
+  # target_group_arns   = module.alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type      = var.instance_type
 
 
 
@@ -79,32 +79,6 @@ module "alb" {
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
-
-  # # Security Group
-  # security_group_ingress_rules = {
-  #   all_http = {
-  #     from_port   = 80
-  #     to_port     = 80
-  #     ip_protocol = "tcp"
-  #     description = "HTTP web traffic"
-  #     cidr_ipv4   = "0.0.0.0/0"
-  #   }
-  #   all_https = {
-  #     from_port   = 443
-  #     to_port     = 443
-  #     ip_protocol = "tcp"
-  #     description = "HTTPS web traffic"
-  #     cidr_ipv4   = "0.0.0.0/0"
-  #   }
-  # }
-  # security_group_egress_rules = {
-  #   all = {
-  #     ip_protocol = "-1"
-  #     cidr_ipv4   = "10.0.0.0/16"
-  #   }
-  # }
-
-
 
   listeners = {
     ex-http = {
@@ -122,7 +96,6 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
